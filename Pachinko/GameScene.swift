4ddacf0d6a26cad70ab3266f102918c8263f320e
noonquiver <camilo.hernandez25@hotmail.com>
2022-07-04 16:00:46 -0500

    Background and some physics and objects added.

---
+++ Pachinko/GameScene.swift
@@ -0,0 +1,32 @@
+//
+//  GameScene.swift
+//  Pachinko
+//
+//  Created by Camilo Hernández Guerrero on 4/07/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+        box.position = location
+        addChild(box)
+    }
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
